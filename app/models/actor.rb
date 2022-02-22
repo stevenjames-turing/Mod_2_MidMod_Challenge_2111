493c83b9c6dd27962f667d59b34dc82fb775c8d4
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-22 14:10:18 -0600

    Extension complete

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,16 @@
 class Actor < ApplicationRecord
     has_many :actor_movies
     has_many :movies, through: :actor_movies
+
+    def co_actors
+        co_actors = []
+        movies.each do |movie|
+            movie.actors.each do |actor|
+                if actor.id != id
+                    co_actors << actor
+                end
+            end
+        end
+        co_actors.uniq
+    end
 end
---
+++ spec/features/actors/show_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'actors show' do
+
+    before(:each) do 
+        @studio_1 = Studio.create!(name: "Studio 1", location: "Hollywood")
+        @studio_2 = Studio.create!(name: "Studio 2", location: "Denver")
+        @studio_3 = Studio.create!(name: "Studio 3", location: "New York City")
+        @movie_1 = @studio_1.movies.create!(title: "Movie 1", creation_year: 1987, genre: "Comedy")
+        @movie_2 = @studio_1.movies.create!(title: "Movie 2", creation_year: 1990, genre: "Action")
+        @movie_3 = @studio_2.movies.create!(title: "Movie 3", creation_year: 1999, genre: "Action")
+        @movie_4 = @studio_3.movies.create!(title: "Movie 4", creation_year: 2012, genre: "Thriller")
+        @actor_1 = Actor.create!(name: "Actor 1", age: 36)
+        @actor_2 = Actor.create!(name: "Actor 2", age: 25)
+        @actor_3 = Actor.create!(name: "Actor 3", age: 31)
+        @actor_4 = Actor.create!(name: "Actor 4", age: 40)
+        @actor_5 = Actor.create!(name: "Actor 5", age: 28)
+        @actor_movie_1 = ActorMovie.create!(movie_id: @movie_1.id, actor_id: @actor_1.id) 
+        @actor_movie_2 = ActorMovie.create!(movie_id: @movie_1.id, actor_id: @actor_2.id) 
+        @actor_movie_3 = ActorMovie.create!(movie_id: @movie_1.id, actor_id: @actor_3.id) 
+        @actor_movie_4 = ActorMovie.create!(movie_id: @movie_2.id, actor_id: @actor_1.id) 
+        @actor_movie_5 = ActorMovie.create!(movie_id: @movie_2.id, actor_id: @actor_4.id) 
+        @actor_movie_5 = ActorMovie.create!(movie_id: @movie_3.id, actor_id: @actor_5.id) 
+    end
+
+    it 'shows actors name and age' do 
+        visit "/actors/#{@actor_1.id}/"
+        
+        expect(page).to have_content(@actor_1.name)
+        expect(page).to have_content(@actor_1.age)
+    end
+    
+    it 'has a unique list of all actors this actor has worked with' do 
+        visit "/actors/#{@actor_1.id}/"
+
+        expect(page).to have_content("Co-Actors")
+
+        within "#coactors" do 
+            expect(page).to have_content(@actor_2.name)
+            expect(page).to have_content(@actor_3.name)
+            expect(page).to have_content(@actor_4.name)
+            expect(page).to_not have_content(@actor_5.name)
+        end
+    end
+end 
